fc23087bded01409e4efe66b5507c0dc759924c5
guialmeda2000 <guialmeda2000@gmail.com>
2025-05-20 17:29:38 +0000

    Shadcn components included.

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,53 @@
-import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, type FormEvent, type ChangeEvent } from "react";
+import { Button } from "./components/ui/button";
+import { Input } from "./components/ui/input";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log("Email: ", email);
+    console.log("Senha: ", password);
+  };
+
+// Suggested code may be subject to a license. Learn more: ~LicenseLog:2424261690.
+  const handleEmailChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setEmail(e.target.value);
+  };
+
+  const handlePasswordChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setPassword(e.target.value);
+  };
 
   return (
     <>
-      <div>
-       <form>
-        <input type="text" />
-        <button type="submit">Submit</button>
-       </form>
+      <div className="flex items-center justify-center h-screen">
+        <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
+          <Input
+            type={"email"}
+            placeholder={"Email"}
+            value={email}
+            onChange={handleEmailChange}
+          />
+          <Input
+            type={"password"}
+            placeholder={"Senha"}
+            value={password}
+            onChange={handlePasswordChange}
+          />
+          <Button
+            variant={"outline"}
+            size={"lg"}
+            type={"submit"}
+          >
+            Teste
+          </Button>
+        </form>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
